baab48ec91a969d9db93949cbb5c638deed8f37e
Laura Heritier <laura.heritier@gmail.com>
2022-02-15 16:22:34 -0300

    New campaign

--- pages/index.js
+++ pages/index.js
@@ -5,52 +5,56 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Regalá una biblia</title>
-        <meta name="description" content="Regalá una biblia" />
+        <title>Campaña Vuelta al cole</title>
+        <meta name="description" content="Campaña Vuelta al cole" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="main">
         <h1 className="title">
-          ¡Hola, amigos!
+          ¡Hola! ¿Qué tal?
         </h1>
 
         <p className="description">
-          Iniciamos la campaña <b>Regalá una biblia</b> a fin de comprar biblias para donar a la <a href="http://sierradorada.com.ar/projectos/hogar_de_ninios/">Fundación Sierra Dorada</a> compuesta por tres hogares de niños. <br />
-          Creemos que este es un deseo que nació en el corazón de nuestro Señor y que se podrá concretar con el esfuerzo de todos como cuerpo de Cristo.
-        </p>
+          Iniciamos una nueva campaña para la vuelta al cole de nuestros 
+          amigos que viven en los hogares de 
+          la <a href="https://www.instagram.com/fundacionsierradorada/">
+            Fundación Sierra Dorada</a>.
+                 </p>
 
         <div className="grid">
-          <p className="card">
-            <h2>¿Cuánto donar?</h2>
-            <p>Cada biblia cuesta $400. Sin embargo, el monto que decidan donar será suficiente.</p>
-          </p>
-
+        <div className="card">
+            <h2>¿A dónde va la donación?</h2>
+            <p>El dinero recibido se destinará a la compra de útiles escolares para niños
+              de Primaria y Secundaria. Esto incluye: mochilas, cartucheras, carpetas, cuadernos,
+              lápices, lapiceras, entre otros útiles.</p>
+          </div>
           <div className="card">
             <h2>¿Hasta cuándo hay tiempo de donar?</h2>
-            <p>Receptaremos el dinero hasta el 25 de agosto.</p>
+            <p>Receptaremos el dinero hasta el 31 de marzo de 2022.</p>
           </div>
 
           <div className="card">
             <h2>¿Cómo realizar la donación?</h2>
             <p>Por transferencia bancaria a la siguiente cuenta:</p>
-            <p>Alias: <b>NIEVES.BELEN</b></p>
-            <p>Titular: Noelia Belen Nieves</p>
+            <p>Alias: <b>lauheritier</b></p>
+            <p>Titular: Laura Heritier</p>
+            
           </div>
 
           <div className="card">
             <h2>¿Se puede colaborar de alguna otra manera?</h2>
             <p>
-              Hay otras formas de participar: necesitamos oración por los niños y los voluntarios que trabajan en el Hogar. Nada de esto sería posible sin la guía del Espíritu Santo ¡Contamos con ello!
+              Orar por los niños y los voluntarios que trabajan en los hogares
+              es otra forma de participar.
             </p>
           </div>
         </div>
 
         <div className="footer">
-          <h3>¡Importante!</h3>
-          <small>Al realizar la compra de las biblias, enviaremos el comprobante de pago a todos los que colaboraron económicamente y, por supuesto, las imágenes de la entrega de las biblias :)</small> 
-          <small>Para más información, se pueden comunicar con Belén Nieves.</small>
-        </div>
+        <p>¡Importante! Enviar el comprobante de transferencia al número de WhatsApp <a href="https://wa.me/543518062904" target="_blank">3518062904</a>. Usaremos este medio para enviar las fotos de las donaciones.</p>
+         
+           </div>
       </main>
     </div>
   )
